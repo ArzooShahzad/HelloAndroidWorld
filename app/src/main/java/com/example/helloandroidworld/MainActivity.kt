a36b03438ebd9522d686abe75f6bc535a476d291
ArzooShahzad <arzoo.shahzad@howardcc.edu>
2022-03-23 23:58:50 -0400

    initial commit

---
+++ app/src/main/java/com/example/helloandroidworld/MainActivity.kt
@@ -0,0 +1,16 @@
+/**
+    MainActivity is an entry point for my app. When I build and run my app,
+    the system launches an instance of this Activity and loads its layout on the device.
+
+ */
+package com.example.helloandroidworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
